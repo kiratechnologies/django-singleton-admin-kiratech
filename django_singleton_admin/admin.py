8f3d1f2664c0d3bc00aceee5f84b75599d49dacd
porowns <porowns@gmail.com>
2020-08-23 18:55:55 -0400

    Fix bug where migrations would fail

--- django_singleton_admin/admin.py
+++ django_singleton_admin/admin.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.http import HttpResponseRedirect
+from django.db.utils import OperationalError
+import logging 
+
+logger = logging.getLogger(__name__) 
 
 class DjangoSingletonModelAdmin(admin.ModelAdmin):
     change_form_template = "django_singleton_admin/change_form.html"
@@ -13,20 +17,23 @@ class DjangoSingletonModelAdmin(admin.ModelAdmin):
 
     def get_urls(self):
         urls = super(DjangoSingletonModelAdmin, self).get_urls()
-
-        model_name = self.model._meta.model_name
-        self.model._meta.verbose_name_plural = self.model._meta.verbose_name
-        url_name_prefix = '%(app_name)s_%(model_name)s' % {
-            'app_name': self.model._meta.app_label,
-            'model_name': model_name,
-        }
-        custom_urls = [
-            url(r'^$',
-                self.admin_site.admin_view(self.change_view),
-                {'object_id': str(self.singleton_instance_id)},
-                name='%s_change' % url_name_prefix),
-        ]
-        # By inserting the custom URLs first, we overwrite the standard URLs.
+        try:
+            model_name = self.model._meta.model_name
+            self.model._meta.verbose_name_plural = self.model._meta.verbose_name
+            url_name_prefix = '%(app_name)s_%(model_name)s' % {
+                'app_name': self.model._meta.app_label,
+                'model_name': model_name,
+            }
+            custom_urls = [
+                url(r'^$',
+                    self.admin_site.admin_view(self.change_view),
+                    {'object_id': str(self.singleton_instance_id)},
+                    name='%s_change' % url_name_prefix),
+            ]
+            # By inserting the custom URLs first, we overwrite the standard URLs.
+        except OperationalError:
+            logger.warning("Django Singleton failed to build URLs, likely because a database migration is required.")
+            custom_urls = []
         return custom_urls + urls
     
     def response_change(self, request, obj):
